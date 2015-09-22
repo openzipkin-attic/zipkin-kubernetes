b82bf3dbcf4c0c9a6510fcb10a6010102ef0d1bf
Dana NicCaluim <dniccaluim@airware.com>
2015-09-21 17:41:10 -0700

    update image paths, shebangs, environment variables and cassandra lifecycle config

--- query/boot.sh
+++ query/boot.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 export CASSANDRA_CONTACT_POINTS=${CASSANDRA_SERVICE}.${POD_NAMESPACE}.cluster.local
 exec /usr/local/bin/run.sh
--- web/boot.sh
+++ web/boot.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 export QUERY_PORT_9411_TCP_ADDR=${QUERY_SERVICE}.${POD_NAMESPACE}.cluster.local
 exec /usr/local/bin/run
